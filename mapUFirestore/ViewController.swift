a9dd00c8c26a94b6f49c65c80c731198bfab9ab7
fofo <fofonorman@gmail.com>
2020-11-04 21:13:56 +0800

    GenQRCode practice

--- mapUFirestore/ViewController.swift
+++ mapUFirestore/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var GenQRcode: UIButton!
+    @IBOutlet weak var QRCodePic: UIImageView!
+    @IBOutlet weak var URLInput: UITextField!
+    var qrcodeImage: CIImage!
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,5 +27,27 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func genQRCodeBtn(_ sender: UIButton) {
+        
+        if qrcodeImage == nil {
+                if URLInput.text == "" {
+                    URLInput.text = "Please input URL"
+                }
+        }
+        
+        let data = URLInput.text!.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
+
+                let filter = CIFilter(name: "CIQRCodeGenerator")
+
+                filter?.setValue(data, forKey: "inputMessage")
+                filter?.setValue("Q", forKey: "inputCorrectionLevel")
+
+        qrcodeImage = filter?.outputImage
+        QRCodePic.image = UIImage(ciImage: qrcodeImage)
+
+        URLInput.resignFirstResponder()
+    }
+    
+    
 }
 
