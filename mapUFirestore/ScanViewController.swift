5014184cda90a70c84f9ed662114bd3e5367d059
fofo <fofonorman@gmail.com>
2020-11-05 18:10:10 +0800

    scanQRCode practice

--- mapUFirestore/ScanViewController.swift
+++ mapUFirestore/ScanViewController.swift
@@ -9,36 +9,76 @@ import UIKit
 import AVFoundation
 
 
-class ScanViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+class ScanViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var camView: UIView!
+    
+    
     var captureSession:AVCaptureSession?
     var PreviewLayer:AVCaptureVideoPreviewLayer?
-    var camWindow:UIView?
+    var qrCodeFrameView:UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return}
         
-        let videoInput: AVCaptureDeviceInput
-        do {
-            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
-                        
-        }catch let error {
-            print(error)
-            return
-        }
+              
         
-        if captureSession?.canAddInput(videoInput) ?? false {
-            captureSession?.addInput(videoInput)
-        }else{
-            return
-        }
         
     }
     
+    
+    //掃QRCode的動作
+    func setQRCodeScan(){
+            
+            //實體化一個AVCaptureSession物件
+            captureSesion = AVCaptureSession()
+            
+            //AVCaptureDevice可以抓到相機和其屬性
+            guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {return}
+            let videoInput:AVCaptureDeviceInput
+            do {
+                videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+            }catch let error {
+                print(error)
+                return
+            }
+            if (captureSesion?.canAddInput(videoInput) ?? false ){
+                captureSesion?.addInput(videoInput)
+            }else{
+                return
+            }
+            
+            //AVCaptureMetaDataOutput輸出影音資料，先實體化AVCaptureMetaDataOutput物件
+            let metaDataOutput = AVCaptureMetadataOutput()
+            if (captureSesion?.canAddOutput(metaDataOutput) ?? false){
+                captureSesion?.addOutput(metaDataOutput)
+                
+                //關鍵！執行緒處理QRCode
+                metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+                //metadataOutput.metadataObjectTypes表示要處理哪些類型的資料，處理QRCODE
+                metaDataOutput.metadataObjectTypes = [.qr, .ean8 , .ean13 , .pdf417]
+                
+            }else{
+                return
+            }
+            
+            //用AVCaptureVideoPreviewLayer來呈現Session上的資料
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSesion!)
+            //顯示size
+            previewLayer.videoGravity = .resizeAspectFill
+            //呈現在camView上面
+            previewLayer.frame = camView.layer.frame
+            //加入畫面
+            view.layer.addSublayer(previewLayer)
+            //開始影像擷取呈現鏡頭的畫面
+            captureSesion?.startRunning()
+        }
+    
+
     
     
 
