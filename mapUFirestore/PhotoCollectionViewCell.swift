238ee81cffa911e48ef66f500f01b7056c79b3fb
fofo <fofonorman@gmail.com>
2020-12-07 21:16:21 +0800

    collectionViewCell Size adjust practice

---
+++ mapUFirestore/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  mapUFirestore
+//
+//  Created by ting pan on 2020/12/7.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    
+}
