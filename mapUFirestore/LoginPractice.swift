9768bfc456277fec0af55ccd3dc27de64bbe51b7
fofo <fofonorman@gmail.com>
2021-01-04 21:55:51 +0800

    國碼選單前端實作完成

--- mapUFirestore/LoginPractice.swift
+++ mapUFirestore/LoginPractice.swift
@@ -7,21 +7,45 @@
 
 import UIKit
 import FirebaseAuth
+import CoreTelephony
 
-class LoginPractice: UIViewController {
+class LoginPractice: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+  
+    
 
     @IBOutlet weak var PhoneNumberField: UITextField!
     @IBOutlet weak var VerificationField: UITextField!
-    
     @IBOutlet weak var loginStatusLabel: UILabel!
+    @IBOutlet weak var countryPicker: UIPickerView!
+    @IBOutlet weak var countryCode: UITextField!
+    
+    let countryArray: [(countryName: String, countryCode: String)] = [("AA", "11"), ("BB", "22"), ("CC", "33"), ("DD", "44"), ("EE", "55"), ("FF", "66")]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        updateUserStatus()
+        countryCode.text = countryArray[0].countryCode
+        countryCode.inputView = countryPicker
+        
+//        updateUserStatus()
         // Do any additional setup after loading the view.
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countryArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countryArray[row].countryName
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        countryCode.text = countryArray[row].countryCode
+    }
     
     @IBAction func checkPhoneNumber(_ sender: Any) {
         if let phoneNumber = PhoneNumberField.text {
@@ -47,7 +71,7 @@ class LoginPractice: UIViewController {
             let credential = PhoneAuthProvider.provider().credential(withVerificationID: verificationID, verificationCode: verificationCode)
             Auth.auth().signIn(with: credential, completion: {(user, error) in
                 if error == nil {
-                    self.updateUserStatus()
+//                    self.updateUserStatus()
                     self.view.endEditing(true)
                 } else {
                     print("login failed!!")
@@ -62,27 +86,28 @@ class LoginPractice: UIViewController {
         
         do {
             try Auth.auth().signOut()
-                updateUserStatus()
+//                updateUserStatus()
            } catch {
             print(error.localizedDescription)
         }
     }
     
-    
-    
-    func updateUserStatus() {
-        if let user = Auth.auth().currentUser {
-            let last4Char = (user.phoneNumber! as NSString).substring(from: 9)
-            
-            loginStatusLabel.text = "已登入，電話末四碼 \(last4Char)"
-                   
-        } else {
-            loginStatusLabel.text = "請登入"
-        }
-        
-    }
-    
 
+    
+//    func updateUserStatus() {
+//        if let user = Auth.auth().currentUser {
+//            let last4Char = (user.phoneNumber! as NSString).substring(from: 9)
+//
+//            loginStatusLabel.text = "已登入，電話末四碼 \(last4Char)"
+//
+//        } else {
+//            loginStatusLabel.text = "請登入"
+//        }
+//
+//    }
+    
+  
+    
     /*
     // MARK: - Navigation
 
